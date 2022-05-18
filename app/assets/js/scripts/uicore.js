368df4a147414ff6e683e35a6ff71bfc75e1973d
user <hankaru724@naver.com>
2022-05-18 18:03:35 +0900

    v1.0.0

--- app/assets/js/scripts/uicore.js
+++ app/assets/js/scripts/uicore.js
@@ -57,7 +57,7 @@ if(!isDev){
                 loggerAutoUpdaterSuccess.log('New update available', info.version)
                 
                 if(process.platform === 'darwin'){
-                    info.darwindownload = `https://github.com/karu724/HoneyBee-1/releases/download/v${info.version}/Bee-Launcher-setup-${info.version}${process.arch === 'arm64' ? '-arm64' : '-x64'}.dmg`
+                    info.darwindownload = `https://github.com/karu724/HoneyBeeL/releases/download/v${info.version}/Bee-Launcher-setup-${info.version}${process.arch === 'arm64' ? '-arm64' : '-x64'}.dmg`
                     showUpdateUI(info)
                 }
                 
